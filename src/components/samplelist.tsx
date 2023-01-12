122713c2e2843066499d5d79f4f4f5e80c9cbf8c
Uran Mikami <122005359+mymelo-s2@users.noreply.github.com>
2023-01-12 13:45:25 +0900

    fix

--- src/components/samplelist.tsx
+++ src/components/samplelist.tsx
@@ -1,11 +1,13 @@
 import { useOutletContext } from "react-router-dom";
 import { useEffect } from "react";
 import "../css/styles.css";
-import Data from "../json/todo.json";
+import Data from "../json/plan.json";
 
 export default function SampleList() {
   const { userName } = useOutletContext<{ userName: string }>();
   const melo = userName;
+  const result = Data.filter((data: any) => data.name === userName);
+
   useEffect(() => {
     const userInfo = Data.filter((data: any) => {
       console.log(data.name);
@@ -13,7 +15,7 @@ export default function SampleList() {
       return data.name === userName;
     });
     console.log(Data);
-    console.log(userInfo[0].TODO);
+    console.log(userInfo[0].plan.d20230101);
   });
   return (
     <div className="SampleList">
@@ -21,7 +23,7 @@ export default function SampleList() {
       <div>sample1: input Text & Props</div>
       <div>sample2: input Date</div>
       <div>sample3: input Password</div>
-      <div>{userName}</div>
+      <div>{result[0].plan.d20230101}</div>
       <div>melo {melo}</div>
     </div>
   );
